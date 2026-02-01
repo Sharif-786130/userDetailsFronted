b9d722afdc7d1dcd5556fc95074fea96cbf32dc2
Sharif-786130 <moulasharif7861@gmail.com>
2026-02-01 08:42:22 +0530

    updated the AuthThunk file

--- src/Features/Auth/authThunk.jsx
+++ src/Features/Auth/authThunk.jsx
@@ -1,12 +1,13 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 export const loginUser = createAsyncThunk(
   "auth/login",
   async (credentials, { rejectWithValue }) => {
     try {
       const response = await axios.post(
-        "http://localhost:8080/api/login",
+       `${API_BASE_URL}/api/login`,
         credentials
       );
       return response.data;
